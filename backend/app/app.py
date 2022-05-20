c2b60e79e83f1ed3c0cb0ba205528b9d7e076b12
“lamvinh1001” <builamvinh2000@gmail.com>
2022-05-20 15:11:03 +0700

    add predict funtion p2p

--- backend/app/app.py
+++ backend/app/app.py
@@ -6,9 +6,20 @@ from utils import extract_face, generate, predict_image
 from io import BytesIO
 from PIL import Image
 from gfpgan import GFPGANer
-# from basicsr.archs.rrdbnet_arch import RRDBNet
 import tensorflow as tf
 from mtcnn import MTCNN
+import botocore
+import boto3
+from werkzeug.utils import secure_filename
+
+BUTKET_NAME = 'demokltn'
+
+ACCESS_KEY_ID = 'AKIAZTKA3HOVTB3CEEBK'
+ACCESS_SECRET_KEY = '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'
+s3 = boto3.client('s3',
+                  aws_access_key_id=ACCESS_KEY_ID,
+                  aws_secret_access_key=ACCESS_SECRET_KEY)
+
 app = Flask(__name__)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
@@ -26,17 +37,6 @@ face_enhancer = GFPGANer(
     upscale=2.5,
     model_path='weights/GFPGANv1.3.pth')
 
-# model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64,
-#                 num_block=23, num_grow_ch=32, scale=4)
-# netscale = 4
-# model_path = (
-#     'weights/RealESRGAN_x4plus.pth')
-# # restorer
-# upsampler = RealESRGANer(
-#     scale=netscale,
-#     model_path=model_path,
-#     model=model)
-
 
 @app.route('/')
 def index():
@@ -46,23 +46,29 @@ def index():
 @app.route('/generate', methods=['POST'])
 def save_base64():
     image_base64 = str(request.form['base64'])
+    image_uri = str(request.form['uri'])
+    name = secure_filename(image_uri.split('/')[-1])
     result_base64 = ''
     original_img = Image.open(BytesIO(base64.b64decode(image_base64)))
+    original_img.save(name)
     try:
         face_img = extract_face(original_img, detector)
 
         img_generate = generate(face_img, generator)
+        #  img_generate = predict_image(generator, face_img)
+        # pill_imgG = Image.fromarray(np.array(img_pre[0]*255, dtype='uint8'))
         _, _, img_enhance = face_enhancer.enhance(
             (img_generate * 255).astype(np.uint8)[:, :, ::-1], has_aligned=False, only_center_face=False, paste_back=True)
         #pill_imgG = Image.fromarray((img_generate * 255).astype(np.uint8))
         pill_imgE = Image.fromarray(img_enhance[:, :, [2, 1, 0]])
         buffered = BytesIO()
         pill_imgE.save(buffered, format="JPEG")
+
+        s3.upload_file(Bucket=BUTKET_NAME, Filename=name,
+                       Key='images/' + name)
         result_base64 = base64.b64encode(buffered.getvalue())
     except:
         print('Error!!')
-        # img_generate = predict_image(generator, face_img)
-        # pill_imgG = Image.fromarray(np.array(img_pre[0]*255, dtype='uint8'))
 
     return result_base64
     # return jsonify({'result': 'Done'})
